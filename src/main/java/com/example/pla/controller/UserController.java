08224ab35858adff36aed200d3f54d16fb95035d
gvsn970 <32327709+gvsn970@users.noreply.github.com>
2023-05-19 10:32:44 +0530

    front end dded

--- src/main/java/com/example/pla/controller/UserController.java
+++ src/main/java/com/example/pla/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.pla.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,14 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	@GetMapping("/")
+    public String viewHomePage(Model model) {
+        return "index";
+    }
+ 
 
-	@GetMapping
+	@GetMapping("/user")
 	public String  getAllUsers() {
 		List<User> usersList =  userService.getAllUser();
 
